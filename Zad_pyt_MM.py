bde3940a68006aa86c347eb5987cf6b3997c743a
Cynamon224 <martixd224@gmail.com>
2022-10-24 00:36:00 +0200

    plik 1 poprawiony

--- Zad_pyt_MM.py
+++ Zad_pyt_MM.py
@@ -4,8 +4,8 @@ Created on Mon Oct 10 13:50:56 2022
 
 @author: Martyna
 """
-#Zad 2
-#a
+# Zad 2
+# a
 '''
 def imiona(lista):
     for i in lista:
@@ -14,44 +14,40 @@ def imiona(lista):
 names = ['Martin', 'Tom', 'Eva','Julia','Julianna']
 imiona (names)
 '''
-#b
+# b
+
 
 def liczby(lista):
     for i in range(len(lista)):
-        lista[i]=lista[i]*2
+        lista[i] = lista[i]*2
     return lista
 
-numerki=[1,2,3,4,5]
+
+numerki = [1, 2, 3, 4, 5]
 print(liczby(numerki))
 
-numerki=[1,2,3,4,5]
-numerki_2=[number*2 for number in numerki]
+numerki = [1, 2, 3, 4, 5]
+numerki_2 = [number*2 for number in numerki]
 print(numerki_2)
 
-#c
+# c
+
 
 def parzyste(liczby):
     for i in range(len(liczby)):
-        if liczby[i]%2==0:
+        if liczby[i] % 2 == 0:
             print(liczby[i])
-            
-numerki10=[1,2,3,4,5,6,7,8,9,10]
+
+
+numerki10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 parzyste(numerki10)
 
-#d
+# d
+
+
 def co_drugie(liczby):
-    print(numerki10[::2]) 
-        
-  
-        
+    print(numerki10[::2])
+
+
 co_drugie(numerki10)
-
-
-
-
-
-
-
-
-
